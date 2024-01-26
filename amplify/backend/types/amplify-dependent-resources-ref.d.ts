1de2347b56e9912cf0582a46c795f95b165c4de6
chrlspdrsn <61101059+chrlspdrsn@users.noreply.github.com>
2024-01-26 14:42:03 -0800

    Add authentication to application

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,13 @@
+export type AmplifyDependentResourcesAttributes = {
+  "auth": {
+    "charlespedersoncom0bc22631": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  }
+}
