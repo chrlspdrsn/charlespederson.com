fc0cc838493f24d38086331f9ac1d7bd5b356dc3
chrlspdrsn <61101059+chrlspdrsn@users.noreply.github.com>
2024-02-09 15:40:07 -0800

    Remove unused component pages

--- src/App.js
+++ src/App.js
@@ -3,29 +3,9 @@ import { Image, Heading, View, Card, } from "@aws-amplify/ui-react";
 
 import NavBar from "./components/NavBar";
 import About from "./components/About";
-import Projects from "./pages/Projects";
-import Contact from "./pages/Contact";
-import Error from "./pages/Error";
 import Home from "./components/Home";
 
 function App() {
-  let Component;
-
-  switch(window.location.pathname) {
-    case "/":
-      Component = About;
-      break;
-    case "/contact":
-      Component = Contact;
-      break;
-    case "/projects":
-      Component = Projects;
-      break;
-    default:
-      Component = Error;
-      break;
-  }
-
   return (
     <div>
       <NavBar />
